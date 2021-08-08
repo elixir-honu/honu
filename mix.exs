3e2604e42d236b2ee0ad771cf462ff5726535d95
0urobor0s <0urobor0s@users.noreply.github.com>
2021-08-08 01:30:22 +0100

    Hex metadata

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule Honu.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/elixir-honu/honu"
+  @version "0.1.0"
+
   def project do
     [
       app: :honu,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Honu",
+      source_url: @source_url
     ]
   end
 
@@ -25,6 +33,18 @@ defmodule Honu.MixProject do
       {:ecto, "~> 3.0"},
       # Web
       {:plug, "~> 1.12"}
+    ]
+  end
+
+  defp description do
+    "A file attachment library for Ecto."
+  end
+
+  defp package do
+    [
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE* CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end

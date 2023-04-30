dfeb0bb318660004c7c631f055595d8a8c47ffe8
0urobor0s <0urobor0s@users.noreply.github.com>
2023-04-30 16:04:50 +0100

    CI

--- config/test.exs
+++ config/test.exs
@@ -3,11 +3,21 @@ import Config
 config :honu,
   ecto_repos: [HonuTest.Repo]
 
-config :honu, HonuTest.Repo,
-  hostname: "localhost",
-  database: "honu_test",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+if System.get_env("CI") do
+  config :honu, HonuTest.Repo,
+    username: System.get_env("POSTGRES_USERNAME", "postgres"),
+    password: System.get_env("POSTGRES_PASSWORD", "postgres"),
+    hostname: "localhost",
+    database: "honu_test#{System.get_env("MIX_TEST_PARTITION")}",
+    pool: Ecto.Adapters.SQL.Sandbox,
+    pool_size: 10
+else
+  config :honu, HonuTest.Repo,
+    hostname: "localhost",
+    database: "honu_test#{System.get_env("MIX_TEST_PARTITION")}",
+    pool: Ecto.Adapters.SQL.Sandbox,
+    pool_size: 10
+end
 
 config :honu, Honu.Storage,
   # TODO: Add in memory storage behaviour or clean directory after test

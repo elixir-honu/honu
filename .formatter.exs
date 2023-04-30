83417e0c5f60d2855d366002eb6960e0046188d2
0urobor0s <0urobor0s@users.noreply.github.com>
2023-04-30 17:41:05 +0100

    Schema DSL to .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,16 @@
+locals_without_parens = [
+  # Schema
+  has_one_attached: 2,
+  has_one_attached: 3,
+  has_many_attached: 2,
+  has_many_attached: 3
+]
+
 [
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ],
   import_deps: [:ecto],
   inputs: ["*.{ex,exs}", "{lib,test}/**/*.{ex,exs}"]
 ]
